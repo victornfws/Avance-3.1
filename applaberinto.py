0e0babf540e5716fdbb2abfe08b862e941e841e7
Victor A. <victor.amaya@u-erre.mx>
2026-04-19 19:29:32 -0600

    back

--- applaberinto.py
+++ applaberinto.py
@@ -116,8 +116,7 @@ def render_maze(maze_np, start, end, ruta_set=None):
             else:
                 fila_str += "⬛"
         filas.append(fila_str)
-    for fila in filas:
-        st.text(fila)
+    st.markdown("<br>".join(filas), unsafe_allow_html=True)
 
 st.title("Visualizador de Algoritmo de Busqueda de Laberinto")
 
